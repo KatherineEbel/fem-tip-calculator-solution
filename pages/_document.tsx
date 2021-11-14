8d329475b21612ec0b83f3cba890ffc96b65d586
Katherine Ebel <kathyebel@me.com>
2021-11-14 12:24:53 -0500

    fix link tag in head of document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            crossOrigin="anonymous"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
